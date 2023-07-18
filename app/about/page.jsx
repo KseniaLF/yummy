1802ac44b46e89c85f001a4882badec9c04d2a07
KseniaLF <llf9391@gmail.com>
2023-07-18 17:41:25 +0300

    add Link to pokemon and  loading

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,5 +1,9 @@
+import Link from "next/link";
+
 async function getPokemons() {
-  const res = await fetch(`https://pokeapi.co/api/v2/pokemon`);
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon`, {
+    next: { revalidate: 10 },
+  });
   return res.json();
 }
 
@@ -12,7 +16,9 @@ export default async function About() {
       About Page
       <ul>
         {results.map((pokemon) => (
-          <li key={pokemon.name}>{pokemon.name}</li>
+          <li key={pokemon.name}>
+            <Link href={`/about/${pokemon.name}`}>{pokemon.name}</Link>
+          </li>
         ))}
       </ul>
     </>
