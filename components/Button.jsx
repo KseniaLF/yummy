3c66c6e5f01c77bd87e97414c7dca8ffb8d157df
KseniaLF <llf9391@gmail.com>
2023-07-20 09:18:15 +0300

    fix input stules

--- components/Button.jsx
+++ components/Button.jsx
@@ -2,7 +2,7 @@ export const Button = ({ children, className }) => {
   return (
     <button
       type="button"
-      className={`text-white custom-rounded bg-second px-12 lg:px-14 py-4 md:py-5 lg:py-6 hover-bg ${className}`}
+      className={`text-white custom-rounded bg-second px-8 md:px-10 lg:px-14 py-4 md:py-5 lg:py-6 hover-bg ${className}`}
     >
       {children}
     </button>
