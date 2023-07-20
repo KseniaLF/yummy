e364f46783553a8e2d210f11d0d4f718572f7932
KseniaLF <llf9391@gmail.com>
2023-07-20 09:02:59 +0300

    add cursive font

--- app/page.js
+++ app/page.js
@@ -7,8 +7,8 @@ export default function Home() {
       <div className="custom-bg"></div>
 
       <div className="pt-14 md:pt-36 lg:pt-44 container text-center md:text-start">
-        <h1 className="text-6xl md:text-7xl lg:text-8xl">
-          <span className="text-main ">So</span>Yummy
+        <h1 className="  text-6xl md:text-7xl lg:text-8xl">
+          <span className="text-main">So</span>Yummy
         </h1>
 
         <p className="max-w-[350px] lg:max-w-[500px]  mt-4">
