0c53164afd7ecaa93a0bc17c60dfee44148e2d86
KseniaLF <llf9391@gmail.com>
2023-07-20 23:19:16 +0300

    add categories slider-interactive

--- app/categories/page.jsx
+++ app/categories/page.jsx
@@ -1,33 +1,75 @@
+"use client";
+
 import { SectionTitle } from "@/components/UI/SectionTitle";
 import { categories } from "../../components/dish-list/data.json";
 import { CategoriesList } from "./CategoriesList";
+import { Suspense, useEffect, useState } from "react";
 
 async function getPokemons() {
   try {
     const promiseA = new Promise((resolve) => {
       resolve(categories);
     });
-    return await promiseA;
+    return promiseA;
   } catch (error) {
     console.error(error);
   }
 }
 
-export default async function About() {
-  const recipes = await getPokemons();
-  console.log(recipes);
+function getCategories() {
+  return categories;
+}
+
+export default function About() {
+  const [activeCategory, setActiveCategory] = useState("Breakfast");
+  const [categoryArr, setCategoryArr] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const categoriesArr = getCategories();
+
+  useEffect(() => {
+    setIsLoading(true);
+    const fetch = async () => {
+      // query for category data should be here
+      const recipes = await getPokemons();
+      const filteredArr = recipes.find((i) => i.name === activeCategory);
+
+      setCategoryArr(filteredArr);
+      setIsLoading(false);
+    };
+
+    fetch();
+  }, [activeCategory]);
+
+  const hanadleClick = (name) => {
+    return setActiveCategory(name);
+  };
 
   return (
     <main className="container">
       <SectionTitle>Categories</SectionTitle>
 
       <ul className="flex gap-2">
-        {recipes.map((recipe) => (
-          <li key={recipe.name}>{recipe.name}</li>
+        {categoriesArr.map(({ name }) => (
+          <li key={name}>
+            <button
+              onClick={() => hanadleClick(name)}
+              type="button"
+              className={`p-2 ${name === activeCategory && "text-main"}`}
+            >
+              {name}
+            </button>
+          </li>
         ))}
       </ul>
 
-      <CategoriesList recipeArr={recipes[0].recipes} />
+      {isLoading ? (
+        <>LOAD</>
+      ) : (
+        categoryArr.length !== 0 && (
+          <CategoriesList recipeArr={categoryArr.recipes} />
+        )
+      )}
     </main>
   );
 }
