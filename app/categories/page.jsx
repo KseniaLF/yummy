eb51b427f79713910bf8d2adbce172f1ad8774d9
KseniaLF <llf9391@gmail.com>
2023-07-25 14:45:05 +0300

    add skeleton to categories-page

--- app/categories/page.jsx
+++ app/categories/page.jsx
@@ -1,3 +1,18 @@
-export default function Categories() {
-  return <>categories</>;
+import { SectionTitleSkeleton } from "@/components/UI/SectionTitle";
+import { Skeleton } from "@/components/UI/skeleton";
+
+export default function Categories(props) {
+  console.log(props.params.id);
+
+  return (
+    <div className="container">
+      <SectionTitleSkeleton />
+
+      <div className="flex gap-3 overflow-x-auto whitespace-nowrap pb-2">
+        {Array.from({ length: 4 }, (_, i) => i + 1).map((id) => (
+          <Skeleton key={id} className="w-[100px] h-[30px] m-[8px]" />
+        ))}
+      </div>
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: ["class"],
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}",
+    "./app/**/*.{js,jsx}",
+    "./src/**/*.{js,jsx}",
   ],
   theme: {
     extend: {
@@ -11,6 +13,20 @@ module.exports = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
     container: {
@@ -30,5 +46,5 @@ module.exports = {
       lg: "1280px",
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
