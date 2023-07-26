8a5c2eede5b3ccb1b13c0888ac3c42b184afa3f5
KseniaLF <llf9391@gmail.com>
2023-07-26 14:08:48 +0300

    add menubar

--- components/nav/Burger.jsx
+++ components/nav/Burger.jsx
@@ -1,7 +1,41 @@
+"use client";
+
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarTrigger,
+} from "@/components/ui/menubar";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const navItems = [
+  { label: "Home", href: "/" },
+  { label: "About", href: "/about/breakfast" },
+  { label: "Categories", href: "/categories/breakfast" },
+];
+
 export const Burger = () => {
+  const pathname = usePathname();
+
   return (
-    <button className="block lg:hidden" type="button">
-      Burger
-    </button>
+    <Menubar className="block lg:hidden" type="button">
+      <MenubarMenu>
+        <MenubarTrigger>Menu</MenubarTrigger>
+        <MenubarContent>
+          {navItems.map((item) => {
+            const isActive = pathname === item.href;
+            return (
+              <MenubarItem key={item.label}>
+                <Link className={isActive ? "text-main" : ""} href={item.href}>
+                  {item.label}
+                </Link>
+              </MenubarItem>
+            );
+          })}
+        </MenubarContent>
+      </MenubarMenu>
+    </Menubar>
   );
 };
