b7665db0da05f17b7d08aa542b633b5ba453332d
KseniaLF <llf9391@gmail.com>
2023-08-01 12:03:50 +0300

    add hide-text class

--- components/my-recipes/Card.jsx
+++ components/my-recipes/Card.jsx
@@ -4,36 +4,39 @@ import { LuTrash2 } from "react-icons/lu";
 export const MyRecipeCard = ({ recipe }) => {
   return (
     <div className="flex gap-3.5 md:gap-7 lg:gap-14 py-3 px-2 md:p-6 lg:p-10 justify-center rounded-lg bg-white">
-      <div className="w-[55vw] max-w-[500px] min-w-[124px]">
+      <div className="w-[55vw] max-w-[500px] min-w-[124px] md:min-w-[230px] lg:min-w-[320px]">
         {recipe.image && (
           <Image
             src={recipe.image}
             alt="recipe image"
             width={500}
             height={500}
-            className="rounded-lg object-cover w-full h-auto"
+            className="rounded-lg object-cover w-full h-full"
           />
         )}
       </div>
 
-      <div className="text-xs md:text-sm lg:text-lg between flex-col ">
+      <div className="text-xs md:text-sm lg:text-lg between flex-col gap-3 md:gap-4 lg:gap-5">
         <div className="between">
           <p className="text-sm md:text-2xl">{recipe.dish}</p>
 
           <button
             type="button"
-            className="p-[5px] md:p-2 rounded-md bg-main text-base md:text-xl text-white"
+            className="p-[5px] md:p-2 lg:p-3 rounded-md bg-main text-base md:text-xl lg:text-2xl text-white"
           >
             <LuTrash2 />
           </button>
         </div>
 
-        <p className="">
-          Lorem ipsum dolor sit amet ddddd ddddddddd dddddddddddddddddd
-          ddddddddddddd dddddddddd ddddddddddddddd d dddddddddddddddddd
-          consectetur..
+        <p className="hide-text max-w-[90%]">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem
+          placeat aperiam odio et quisquam unde dolorum quo officiis hic error.
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores
+          doloremque magni maiores molestiae sapiente suscipit explicabo enim
+          quisquam ducimus sed!
         </p>
-        <div className="between items-center">
+
+        <div className="between items-center mt-4">
           <span>20 min</span>
 
           <button
