9f267fcbf1908ce56302217951a15a840f40708d
KseniaLF <llf9391@gmail.com>
2023-08-11 22:08:18 +0300

    add zod schema to recipe-add-page

--- app/add/page.jsx
+++ app/add/page.jsx
@@ -5,22 +5,13 @@ import { SectionTitle } from "@/components/UI/SectionTitle";
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
   FormMessage,
 } from "@/components/UI/form";
 import { Input } from "@/components/UI/input";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/UI/select";
 import { Textarea } from "@/components/UI/textarea";
-import { SelectCategory } from "@/components/add-dish/selectCategory";
 import axios from "axios";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
@@ -28,18 +19,19 @@ import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { toast } from "react-hot-toast";
-
-const categories = ["miscellaneous", "breakfast", "chicken", "desserts"];
-const timesArr = ["less then 20", "30", "60", "more then 65"];
+import { zodResolver } from "@hookform/resolvers/zod";
+import { recipeSchema } from "@/schemas/recipeSchema";
+import { CategoryFormField } from "@/components/form/CategoryFormField";
+import { TimeFormField } from "@/components/form/TimeFormField";
 
 export default function Add() {
   const router = useRouter();
   const { data: session } = useSession();
-  const type = "Create";
 
   const [submitting, setIsSubmitting] = useState(false);
 
   const form = useForm({
+    resolver: zodResolver(recipeSchema),
     defaultValues: {
       dish: "",
       category: "breakfast",
@@ -91,38 +83,12 @@ export default function Add() {
                 <FormControl>
                   <Input placeholder="shadcn" {...field} />
                 </FormControl>
-                <FormDescription>This is your Dish name.</FormDescription>
                 <FormMessage />
               </FormItem>
             )}
           />
 
-          <FormField
-            control={form.control}
-            name="category"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Category</FormLabel>
-                <Select onValueChange={field.onChange} defaultValue="breakfast">
-                  <FormControl>
-                    <SelectTrigger>
-                      <SelectValue placeholder="Select a category" />
-                    </SelectTrigger>
-                  </FormControl>
-
-                  <SelectContent>
-                    {categories.map((category) => (
-                      <SelectItem key={category} value={category}>
-                        {category}
-                      </SelectItem>
-                    ))}
-                  </SelectContent>
-                </Select>
-
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+          <CategoryFormField control={form.control} />
 
           <FormField
             control={form.control}
@@ -142,35 +108,10 @@ export default function Add() {
             )}
           />
 
-          <FormField
-            control={form.control}
-            name="time"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Cooking time</FormLabel>
-                <Select onValueChange={field.onChange} defaultValue="30">
-                  <FormControl>
-                    <SelectTrigger>
-                      <SelectValue placeholder="Select a time" />
-                    </SelectTrigger>
-                  </FormControl>
-
-                  <SelectContent>
-                    {timesArr.map((time) => (
-                      <SelectItem key={time} value={time}>
-                        {time} min
-                      </SelectItem>
-                    ))}
-                  </SelectContent>
-                </Select>
-
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+          <TimeFormField control={form.control} />
 
           <Button type="submit" disabled={submitting}>
-            {submitting ? `Creating...` : type}
+            {submitting ? `Creating...` : "Create"}
           </Button>
         </form>
       </Form>
---
+++ components/form/CategoryFormField.jsx
@@ -0,0 +1,46 @@
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/UI/form";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/UI/select";
+import { categories } from "@/utils/globalData";
+
+export const CategoryFormField = ({ control }) => {
+  return (
+    <FormField
+      control={control}
+      name="category"
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel>Category</FormLabel>
+          <Select onValueChange={field.onChange} defaultValue="breakfast">
+            <FormControl>
+              <SelectTrigger>
+                <SelectValue placeholder="Select a category" />
+              </SelectTrigger>
+            </FormControl>
+
+            <SelectContent>
+              {categories.map((category) => (
+                <SelectItem key={category} value={category}>
+                  {category}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+};
